4636426604ac64beea04fd13473191ad075d5856
carecarestinga <carecarestinga@gmail.com>
2018-04-09 15:23:46 -0300

    Segundo

--- src/main/java/br/edu/ifrs/restinga/carlossoares/ds/lista2/dao/LivroDAO.java
+++ src/main/java/br/edu/ifrs/restinga/carlossoares/ds/lista2/dao/LivroDAO.java
@@ -1,7 +1,8 @@
-
 package br.edu.ifrs.restinga.carlossoares.ds.lista2.dao;
 
 import br.edu.ifrs.restinga.carlossoares.ds.lista2.model.Livro;
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,48 @@ import org.springframework.stereotype.Repository;
  * @author 10070265
  */
 @Repository
-public interface LivroDAO extends CrudRepository<Livro, Integer>{
-    
+public interface LivroDAO extends CrudRepository<Livro, Integer> {
+
+    //pesquisa por titulo
+    Livro findByTitulo(String titulo);
+
+    // Equivalente ao like, mas não precisamo nos preocupar com o sinal de percentual. 
+    // Podemos usar também EndingWith, Containing.
+    List<Livro> findByTituloStartingWith(String titulo);
+
+    // Ordenando pelo nome.
+    List<Livro> findByTituloStartingWithOrderByTitulo(String titulo);
+
+    // Não levar em consideração a caixa.
+    List<Livro> findByTituloStartingWithIgnoreCase(String titulo);
+
+    // Pesquisando por duas propriedades: nome e ativo.
+    List<Livro> findByTituloStartingWithIgnoreCaseAndDoacaoEquals(String titulo, boolean doacao);
+
+    //pesquisa por nome
+    Livro findByAutorPrimeiroNome(String autorPrimeiroNome);
+
+    // Equivalente ao like, mas não precisamo nos preocupar com o sinal de percentual. 
+    // Podemos usar também EndingWith, Containing.
+    List<Livro> findByAutorPrimeiroNomeStartingWith(String autorPrimeiroNome);
+
+    // Ordenando pelo nome.
+    List<Livro> findByAutorPrimeiroNomeStartingWithOrderByAutorPrimeiroNome(String autorPrimeiroNome);
+
+    // Não levar em consideração a caixa.
+    List<Livro> findByAutorPrimeiroNomeStartingWithIgnoreCase(String autorPrimeiroNome);
+
+    // Pesquisando por duas propriedades: nome e ativo.
+    List<Livro> findByAutorPrimeiroNomeStartingWithIgnoreCaseAndDoacaoEquals(String autorPrimeiroNome, boolean doacao);
+
+    // Nesse caso, precisamos usar o sinal de percentual em nossas consultas.
+    List<Livro> findByAutorPrimeiroNomeLike(String autorPrimeiroNome);
+
+    // Todos com quantidade "menor que". Poderia ser usado
+    // também LessThanEqual, GreaterThan, GreaterThanEqual.
+    List<Livro> findByAnoPublicacaoLessThan(int anoPublicacao);
+
+    // @Query("from Livro")
+    List<Livro> pesquisarLivros();
+
 }
--- src/main/java/br/edu/ifrs/restinga/carlossoares/ds/lista2/dao/UsuarioDAO.java
+++ src/main/java/br/edu/ifrs/restinga/carlossoares/ds/lista2/dao/UsuarioDAO.java
@@ -1,7 +1,9 @@
-
 package br.edu.ifrs.restinga.carlossoares.ds.lista2.dao;
 
 import br.edu.ifrs.restinga.carlossoares.ds.lista2.model.Usuario;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,52 @@ import org.springframework.stereotype.Repository;
  * @author 10070265
  */
 @Repository
-public interface UsuarioDAO extends CrudRepository<Usuario, Integer>{
+public interface UsuarioDAO extends CrudRepository<Usuario, Integer> {
+    // pesquisar por nome
+    Usuario findByNome(String nome);
+
+    // Equivalente ao like, mas não precisamo nos preocupar com o sinal de percentual. 
+    // Podemos usar também EndingWith, Containing.
+    List<Usuario> findByNomeStartingWith(String nome);
+
+    // Ordenando pelo nome.
+    List<Usuario> findByNomeStartingWithOrderByNome(String nome);
+
+    // Não levar em consideração a caixa.
+    List<Usuario> findByNomeStartingWithIgnoreCase(String nome);
+
+    // Pesquisando por duas propriedades: nome e ativo.
+    List<Usuario> findByNomeStartingWithIgnoreCaseAndAtivoEquals(String nome, boolean ativo);
+
+    // Nesse caso, precisamos usar o sinal de percentual em nossas consultas.
+    List<Usuario> findByNomeLike(String nome);
+
+    // Quando você quer negar o que passa no parâmetro
+    List<Usuario> findByNomeNot(String nome);
     
+    // pesquisar por nome
+    Usuario findByCpf(String cpf);
+
+    // Equivalente ao like, mas não precisamo nos preocupar com o sinal de percentual. 
+    // Podemos usar também EndingWith, Containing.
+    List<Usuario> findByCpfStartingWith(String cpf);
+
+    // Ordenando pelo nome.
+    List<Usuario> findByCpfStartingWithOrderByCpf(String cpf);
+
+    // Não levar em consideração a caixa.
+    List<Usuario> findByCpfStartingWithIgnoreCase(String cpf);
+
+    // Nesse caso, precisamos usar o sinal de percentual em nossas consultas.
+    List<Usuario> findByCpfLike(String cpf);
+
+    // Quando você quer negar o que passa no parâmetro
+    List<Usuario> findByCpfNot(String cpf);
+
+    // Todos os usuarios com os IDs passados no varargs. Poderia usar NotIn para negar os IDs.
+    List<Usuario> findByIdIn(int... ids);
+
+    // @Query("from Usuario")
+    List<Usuario> pesquisarUsuarios();
+
 }
